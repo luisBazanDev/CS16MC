0011ef7d041818094c3e64a78381865c43c19012
Luis Bazan <luis@bazan.pe>
2023-09-04 10:56:31 -0500

    Implement configs file in the main class

--- src/main/java/pe/bazan/luis/plugins/cs16mc/CS16MC.java
+++ src/main/java/pe/bazan/luis/plugins/cs16mc/CS16MC.java
@@ -2,13 +2,20 @@ package pe.bazan.luis.plugins.cs16mc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import pe.bazan.luis.plugins.cs16mc.commands.CommandHandler;
+import pe.bazan.luis.plugins.cs16mc.configs.ArenasConfig;
+import pe.bazan.luis.plugins.cs16mc.configs.MessagesConfig;
 import pe.bazan.luis.plugins.cs16mc.events.JoinServer;
 
 public final class CS16MC extends JavaPlugin {
+    private MessagesConfig messagesConfig;
+    private ArenasConfig arenasConfig;
+    private static CS16MC instance;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
+        loadConfigs();
         setUpCommandHandler();
         getServer().getPluginManager().registerEvents(new JoinServer(), this);
     }
@@ -18,7 +25,25 @@ public final class CS16MC extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+    public void loadConfigs() {
+        // Load all config files
+        messagesConfig = new MessagesConfig(this);
+        arenasConfig = new ArenasConfig(this);
+    }
+
     public void setUpCommandHandler() {
         getCommand("cs16mc").setExecutor(new CommandHandler(this));
+    }
+
+    public static CS16MC getInstance() {
+        return instance;
+    }
+
+    public ArenasConfig getArenasConfig() {
+        return arenasConfig;
+    }
+
+    public MessagesConfig getMessagesConfig() {
+        return messagesConfig;
     }
 }
