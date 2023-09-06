298a30942895afd17f4fea1db27318a0e845804d
Luis Bazan <luis@bazan.pe>
2023-09-06 14:40:37 -0500

    Implement RestartHttp plugin's routes for scripts

--- assets/scripts/reloadTestServer.py
+++ assets/scripts/reloadTestServer.py
@@ -7,6 +7,6 @@ load_dotenv()
 SERVER_IP=os.getenv('SERVER_IP')
 SERVER_HTTP_PORT=os.getenv('SERVER_HTTP_PORT')
 
-r = requests.get(f'http://{SERVER_IP}:{SERVER_HTTP_PORT}/restart')
+r = requests.get(f'http://{SERVER_IP}:{SERVER_HTTP_PORT}/reload')
 
 print(r.text)
