6da8408d875438e5ff8a4b0fc3cd133944151c9a
Jonathan-MR19 <jonathanvegeta11@gmail.com>
2023-11-07 11:48:24 -0500

    Code format Gun class

--- src/main/java/pe/bazan/luis/plugins/cs16mc/domain/items/weapons/Gun.java
+++ src/main/java/pe/bazan/luis/plugins/cs16mc/domain/items/weapons/Gun.java
@@ -2,10 +2,8 @@ package pe.bazan.luis.plugins.cs16mc.domain.items.weapons;
 
 import pe.bazan.luis.plugins.cs16mc.domain.items.FireArms;
 
-    public class Gun extends FireArms {
-
-
-        public Gun(float price, float damage, float weight, int rechargeTime, int chargerCapacity, int currentCapacity) {
-            super(price, damage, weight, rechargeTime, chargerCapacity,currentCapacity;
-        }
+public class Gun extends FireArms {
+    public Gun(float price, float damage, float weight, int rechargeTime, int chargerCapacity, int currentCapacity) {
+        super(price, damage, weight, rechargeTime, chargerCapacity,currentCapacity;
     }
+}
